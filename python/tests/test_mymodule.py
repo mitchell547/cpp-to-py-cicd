1b323071b38ab7b578daedb44d00c31dc2c20740
Mikhail <mchalyshev@gmail.com>
2026-02-05 15:17:56 +0300

    add unit tests for python

---
+++ python/tests/test_mymodule.py
@@ -0,0 +1,36 @@
+from mymodule.main import my_sum
+import pytest
+
+def test_empty_arr():
+    res = my_sum([])
+    assert res == 0, "Sum of empty array should be 0"
+    
+def test_all_zeros():
+    res = my_sum([0,0,0,0,0,0,0,0])
+    assert res == 0, "Sum of all zero elements should be 0"
+    
+def test_single_elem():
+    res = my_sum([5])
+    assert res == 5, "Wrong answer"
+    
+def test_ten_elems():
+    res = my_sum([1,2,3,4,5,6,7,8,9,10])
+    assert res == 55, "Wrong answer"
+    
+def test_thousand_elems():
+    arr = [1] * 1000
+    res = my_sum(arr)
+    assert res == 1000, "Wrong answer"
+    
+def test_negative_elems():
+    res = my_sum([-1, -2, -3])
+    assert res == -6, "Wrong answer"
+
+def test_opposite_elems():
+    res = my_sum([-9999, 9999])
+    assert res == 0, "Wrong answer"
+    
+def test_wrong_datatype():
+    with pytest.raises(TypeError):
+        res = my_sum(['a'])
+    
